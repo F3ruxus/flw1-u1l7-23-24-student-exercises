aedb18ab3220acb163cce867a27edc246188c783
F3ruxus <140278358+F3ruxus@users.noreply.github.com>
2023-12-01 00:23:21 +0000

    Added the code to fill in the blanks! :D

--- profile.js
+++ profile.js
@@ -7,10 +7,27 @@ let main = document.querySelector(".main");
 button.addEventListener("click", function() {
 
     // 1. Updated the innerHTML of profileName to your name or a made up one.
-
+  profileName.innerHTML = "Juan";
     // 2. Updated the src of profileImage a picture of you or one that makes sense.
-
+  profileImage.src = "https://www.google.com/url?sa=i&url=https%3A"
     // 3. Updated the src of main to a photo that's be your first post.
-
-
+  main.src = "https://wallup.net/wp-content/uploads/2019/09/295784-mountains-landscapes-nature-california-streams-land.jpg"
 });
+
+
+// String Interpolation example
+let name = 'John';
+let greeting = `Hello, ${name}!`;
+
+console.log(greeting); // Hello, John!
+
+
+// Template Literals example
+let a = 10;
+let b = 20;
+let result = `Sum of a and b is ${a + b}.`;
+console.log(result); // The sum of a and b is 30.
+
+// InnerHTML example
+let name2 = "Sara";
+name2.innerHTML = `Hello, ${name2}!`;
--- userInput.js
+++ userInput.js
@@ -1,21 +1,33 @@
 // button and div element
 let saveButton = document.querySelector('.save-button');
-let displayMessage = document.querySelector('.display-message')
+let displayMessage = document.querySelector('.actual-display-message')
 
 // click event for button
 saveButton.addEventListener('click', function() {
     
 
     // 1. Take a look at the userInput.html file to look over the elements there.
-
+  
 
     // 2. Declare a variable called userInput.
     // - Select the class of the input field using querySelector.
     // - Add the value method.
-
+  var userInput = document.querySelector(".user-input").value;
 
     // 2. Set the innerHTML of the display div to the user input.
-
+  displayMessage.innerHTML = userInput;
 
 
 });
+
+// Storing user data example //
+// let button = document.querySelector("button");
+// button.addEventListener("click", function() {
+//  let userInput = document.querySelector("input").value;
+// };
+
+
+// .value example
+// 1 click event {
+// 2  let userInput = html.value;
+// 3 };
